461d07e92131153dbe071dd1b222a54a96ab9142
Mitch Tabian <mitch@tabian.ca>
2019-10-01 08:34:20 -0700

    video starting point

--- app/src/main/java/com/codingwithmitch/androidimagecropperexample/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/androidimagecropperexample/MainActivity.kt
@@ -1,82 +1,17 @@
 package com.codingwithmitch.androidimagecropperexample
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import com.bumptech.glide.Glide
-import com.theartofdev.edmodo.cropper.CropImage
-import com.theartofdev.edmodo.cropper.CropImageView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = "AppDebug"
-
-    private val GALLERY_REQUEST_CODE = 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        update_textview.setOnClickListener {
-            pickFromGallery()
-        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-            when (requestCode) {
-
-                GALLERY_REQUEST_CODE -> {
-                    if (resultCode == Activity.RESULT_OK) {
-                        data?.data?.let { uri ->
-                            launchImageCrop(uri)
-                        }
-                    }
-                    else{
-                        Log.e(TAG, "Image selection error: Couldn't select that image from memory." )
-                    }
-                }
-
-                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
-                    val result = CropImage.getActivityResult(data)
-                    if (resultCode == Activity.RESULT_OK) {
-                        setImage(result.uri)
-                    }
-                    else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                        Log.e(TAG, "Crop error: ${result.getError()}" )
-                    }
-                }
-            }
-    }
-
-    private fun setImage(uri: Uri){
-        Glide.with(this)
-            .load(uri)
-            .into(image)
-    }
-
-    private fun launchImageCrop(uri: Uri){
-        CropImage.activity(uri)
-            .setGuidelines(CropImageView.Guidelines.ON)
-            .setAspectRatio(1920, 1080)
-            .setCropShape(CropImageView.CropShape.RECTANGLE) // default is rectangle
-            .start(this)
-    }
-
-    private fun pickFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.type = "image/*"
-        val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        startActivityForResult(intent, GALLERY_REQUEST_CODE)
-    }
 }
 
 
